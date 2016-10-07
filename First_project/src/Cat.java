67b2493e8c1ab5cc914fe9754604d4a0976fb05e
natasha <natali.chizhikova.88@mail.ru>
2016-10-07 23:05:30 +0300

    Lection2

---
+++ First_project/src/Cat.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Stas on 07.10.2016.
+ */
+public class Cat {
+    String name;
+    String color;
+
+    void myau(){
+        System.out.println("I am "+name+ " " +color);
+    }
+}
---
+++ First_project/src/Task_1.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Stas on 07.10.2016.
+ */
+public class Task_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        Cat vasya = new Cat();
+        vasya.name = "Vasya";
+        vasya.color = "red";
+        vasya.myau();
+
+        Cat petya = new Cat();
+        petya.name = "Petya";
+        petya.color = "red";
+        petya.myau();
+    }
+}
